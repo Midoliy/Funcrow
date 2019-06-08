f33e048ba1c0cb16e400920065fe1f224bb4f17e
Midoliy <s85f079p@gmail.com>
2019-06-08 10:37:49 +0900

    💡Stateモナドを追加.

---
+++ src/Funcrow.Core/CustomComputations/StateComputation.fs
@@ -0,0 +1,15 @@
+namespace Funcrow.Core
+
+module StateComputation =
+    let private ret x = fun s -> (x, s)
+    let private (>>=) m f = fun s -> let (x, s') = m s
+                                     f x s'
+
+    type StateBuilder () =
+        member __.Bind (m, f) = m >>= f
+        member __.Return (x) = ret x
+
+    let state = StateBuilder ()
+    let evalState m s = fst (m s)
+    let get = fun s -> (s, s)
+    let put s = fun _ -> ((), s)
