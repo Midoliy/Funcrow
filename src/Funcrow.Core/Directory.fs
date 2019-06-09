f568a540df8d9924b81bad0b3cb255609c83a72f
Midoliy <s85f079p@gmail.com>
2019-06-09 21:29:41 +0900

    💡Directoryモジュール修正.

--- src/Funcrow.Core/Directory.fs
+++ src/Funcrow.Core/Directory.fs
@@ -10,38 +10,40 @@ module Directory =
     let create path = 
         path |> exists
              |> function
-                | true -> Some (DirectoryInfo (path))
-                | false -> try Some (Directory.CreateDirectory path) with _ -> None
-    let delete path =
+                | true -> Right (DirectoryInfo (path))
+                | false -> try Right (Directory.CreateDirectory path) with exn as e -> Left e
+    let deleteWithRecursive recursive path =
         path |> exists
              |> function
-                | true -> try Directory.Delete path; Right true with | exn as e -> Left e
+                | true -> try Directory.Delete (path, recursive); Right true with exn as e -> Left e
                 | false -> Right true 
+    let deleteAll path = deleteWithRecursive true path
+    let delete path = deleteWithRecursive false path
     let getFilesWithOptions pattern (depth:SearchOption) path =
         path |> exists
              |> function
-                | true -> Some (Directory.GetFiles (path, pattern, depth))
-                | false -> None
+                | true -> try Right (Directory.GetFiles (path, pattern, depth)) with exn as e -> Left e
+                | false -> Left (Exception "")
     let getFilesWithSearchDepth (depth:SearchOption) path = getFilesWithOptions "*" depth path
     let getFiles path = getFilesWithSearchDepth SearchOption.TopDirectoryOnly path
     let enumerateFilesWithOptions pattern (depth:SearchOption) path =
         path |> exists
              |> function
-                | true -> Some (Directory.EnumerateFiles (path, pattern, depth))
-                | false -> None
+                | true -> try Right (Directory.EnumerateFiles (path, pattern, depth)) with exn as e -> Left e
+                | false -> Left (Exception "")
     let enumerateFilesWithSearchDepth (depth:SearchOption) path = enumerateFilesWithOptions "*" depth path
     let enumerateFiles path = enumerateFilesWithSearchDepth SearchOption.TopDirectoryOnly path
     let getDirectoriesWithOptions pattern (depth:SearchOption) path =
         path |> exists
              |> function
-                | true -> Some (Directory.GetDirectories (path, pattern, depth))
-                | false -> None
+                | true -> try Right (Directory.GetDirectories (path, pattern, depth)) with exn as e -> Left e
+                | false -> Left (Exception "")
     let getDirectoriesWithSearchDepth (depth:SearchOption) path = getDirectoriesWithOptions "*" depth path
     let getDirectories path = getDirectoriesWithSearchDepth SearchOption.TopDirectoryOnly path
     let enumerateDirectoriesWithOptions pattern (depth:SearchOption) path =
         path |> exists
              |> function
-                | true -> Some (Directory.EnumerateDirectories (path, pattern, depth))
-                | false -> None
+                | true -> try Right (Directory.EnumerateDirectories (path, pattern, depth)) with exn as e -> Left e
+                | false -> Left (Exception "")
     let enumerateDirectoriesWithSearchDepth (depth:SearchOption) path = enumerateDirectoriesWithOptions "*" depth path
     let enumerateDirectories path = enumerateDirectoriesWithSearchDepth SearchOption.TopDirectoryOnly path
