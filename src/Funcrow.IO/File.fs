cc0cf05ca87e673ac140cca785f611b208d446b7
midoliy <s85f079p@gmail.com>
2019-07-08 07:00:10 +0900

    💡Funcrow.IO.Fileを更新

--- src/Funcrow.IO/File.fs
+++ src/Funcrow.IO/File.fs
@@ -4,4 +4,49 @@ open System.IO
 open Funcrow.EitherComputation
 
 module File =
-    let exists = File.Exists
+    let appendAllLines contents path = File.AppendAllLines (path, contents)
+
+    let appendAllLinesWith encoding contents path = File.AppendAllLines (path, contents, encoding)
+
+    let appendAllText contents path = File.AppendAllText (path, contents)
+
+    let appendAllTextWith encoding contents path = File.AppendAllText (path, contents, encoding)
+
+    let appendText path = File.AppendText path
+
+    let copy src dst = File.Copy (src, dst)
+    
+    let copyWith overwrite src dst = File.Copy (src, dst, overwrite)
+
+    let create path = File.Create path
+
+    let createWith bufferSize path = File.Create (path, bufferSize)
+
+    let createWithOpt options bufferSize path = File.Create (path, bufferSize, options)
+
+    let createText path = File.CreateText path
+
+    let decrypt path = File.Decrypt path
+
+    let delete path = File.Delete path
+
+    let encrypt path = File.Encrypt path
+
+    let exists path = File.Exists path
+
+    let getAttributes path = File.GetAttributes path
+
+    let getCreationTime path = File.GetCreationTime path
+
+    let getCreationTimeUtc path = File.GetCreationTimeUtc path
+
+    let getLastAccessTime path = File.GetLastAccessTime path
+
+    let getLastAccessTimeUtc path = File.GetLastAccessTimeUtc path
+
+    let getLastWriteTime path = File.GetLastWriteTime path
+
+    let getLastWriteTimeUtc path = File.GetLastWriteTimeUtc path
+
+    let move dst src = File.Move (src, dst)
+
