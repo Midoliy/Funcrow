8740f43f7179631380111caf82e46bfcec29fa98
T.Midorikawa <s85f079p@gmail.com>
2019-07-22 08:19:29 +0900

    💡Update Funcrow.IO.File.fs
    eitherモナドを適用

--- src/Funcrow.IO/File.fs
+++ src/Funcrow.IO/File.fs
@@ -1,52 +1,103 @@
 namespace Funcrow.IO
 
-open System.IO
-open Funcrow.EitherComputation
-
 module File =
-    let appendAllLines contents path = File.AppendAllLines (path, contents)
-
-    let appendAllLinesWith encoding contents path = File.AppendAllLines (path, contents, encoding)
-
-    let appendAllText contents path = File.AppendAllText (path, contents)
-
-    let appendAllTextWith encoding contents path = File.AppendAllText (path, contents, encoding)
-
-    let appendText path = File.AppendText path
-
-    let copy src dst = File.Copy (src, dst)
-    
-    let copyWith overwrite src dst = File.Copy (src, dst, overwrite)
-
-    let create path = File.Create path
-
-    let createWith bufferSize path = File.Create (path, bufferSize)
-
-    let createWithOpt options bufferSize path = File.Create (path, bufferSize, options)
-
-    let createText path = File.CreateText path
-
-    let decrypt path = File.Decrypt path
-
-    let delete path = File.Delete path
-
-    let encrypt path = File.Encrypt path
+    open System.IO
+    open System.Text
+    open Funcrow.EitherComputation
 
     let exists path = File.Exists path
+    
+    let appendAllLinesWith encoding contents path =
+        path
+        |> exists
+        |> function
+           | true -> either { return File.AppendAllLines (path, contents, encoding) }
+           | false -> Left (exn ("File not found. [" + path + "]"))
 
-    let getAttributes path = File.GetAttributes path
+    let appendAllLines contents path  =
+        appendAllLinesWith Encoding.UTF8 contents path
+           
+    let appendAllTextWith encoding contents path = 
+        path
+        |> exists
+        |> function
+           | true -> either { return File.AppendAllText (path, contents, encoding) }
+           | false -> Left (exn ("File not found. [" + path + "]"))
 
-    let getCreationTime path = File.GetCreationTime path
+    let appendAllText contents path =
+        appendAllTextWith Encoding.UTF8 contents path
 
-    let getCreationTimeUtc path = File.GetCreationTimeUtc path
+    let appendText path = 
+        path
+        |> exists
+        |> function
+           | true -> either { return File.AppendText path }
+           | false -> Left (exn ("File not found. [" + path + "]"))
+    
+    let copyWith overwrite src dst = 
+        src
+        |> exists
+        |> function
+           | true -> dst
+                     |> exists
+                     |> function
+                        | true -> Left(exn ("File exists already. [" + dst + "]"))
+                        | false -> either { return File.Copy (src, dst, overwrite) }
+           | false -> Left (exn ("File not found. [" + src + "]"))
 
-    let getLastAccessTime path = File.GetLastAccessTime path
+    let copy src dst =
+        copyWith false src dst 
+        
+    let createWithOpt options bufferSize path : Either<exn,_> =
+        either { return File.Create (path, bufferSize, options) }
 
-    let getLastAccessTimeUtc path = File.GetLastAccessTimeUtc path
+    let createWith bufferSize path : Either<exn,_> =
+        either { return File.Create (path, bufferSize) }
 
-    let getLastWriteTime path = File.GetLastWriteTime path
+    let create path =
+        createWith 4096 path
 
-    let getLastWriteTimeUtc path = File.GetLastWriteTimeUtc path
+    let createText path : Either<exn,_> =
+        either { return File.CreateText path }
 
-    let move dst src = File.Move (src, dst)
+    let decrypt path : Either<exn,_> = 
+        either { return File.Decrypt path }
 
+    let delete path : Either<exn,_> = 
+        either { return File.Delete path }
+
+    let encrypt path : Either<exn,_> = 
+        either { return File.Encrypt path }
+
+    let getAttributes path : Either<exn,_> = 
+        either { return File.GetAttributes path }
+
+    let getCreationTime path : Either<exn,_> = 
+        either { return File.GetCreationTime path }
+
+    let getCreationTimeUtc path : Either<exn,_> = 
+        either { return File.GetCreationTimeUtc path }
+
+    let getLastAccessTime path : Either<exn,_> = 
+        either { return File.GetLastAccessTime path }
+
+    let getLastAccessTimeUtc path : Either<exn,_> = 
+        either { return File.GetLastAccessTimeUtc path }
+
+    let getLastWriteTime path : Either<exn,_> = 
+        either { return File.GetLastWriteTime path }
+
+    let getLastWriteTimeUtc path : Either<exn,_> = 
+        either { return File.GetLastWriteTimeUtc path }
+        
+    let move dst src : Either<exn,_> = 
+        src
+        |> exists
+        |> function
+           | true -> dst
+                     |> exists
+                     |> function
+                        | true -> Left(exn ("File exists already. [" + dst + "]"))
+                        | false -> either { return File.Move (src, dst) }
+           | false -> Left (exn ("File not found. [" + src + "]"))
+           
